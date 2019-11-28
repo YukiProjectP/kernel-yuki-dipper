8144e1c0615961b63ada3333bfb70f273387857b
Tanish Manku <tanish2k09.dev@gmail.com>
2019-11-28 14:10:04 -0500

    Drivers: Introduce K-lapse v5.0
    - Introducing k-lapse "flow", a brightness-based scaling mode QOL enhancement that scales RGB points by 1 smoothly instant of instantly taking effect
    - Major code refactor to better match the Linux Kernel coding style (huge thanks to kdrag0n for initial overall patch and general guidance)
    - Switched from SYS ATTR to module_param_cb and kernel param structs
    - Shortened the code from 1k lines to 0.6k without compromising any logic or feature
    - Changed tunable naming for sysfs to convey the idea of the function in a better way
    - Added minute-level support for automation. Yes, you can now start or stop at, say 16:49
    - Dimmer factor (previously known as brightness_factor) is now allowed to be set as a percentage value, ranging from 10 to 100
    - Updated README documentation to match v5.0 release
    - Integrated MDSS code with a switch in klapse.h (see kcal-mdss-demo branch for full implementation in older version)
    - Changed some default config values
    - Revised dimmer logic to fix "dimmer value multiplicative effect" for some users. The issue was changing brightness_factor values multiple times stacked them on top of each other, eventually causing the RGB to hit their minimum limit (usually 20 in KCAL), and not being able to restore full RGB without a pulse
    
    Signed-off-by: Tanish Manku <tanish2k09.dev@gmail.com>
    [kingbri: Add K-Lapse's README into Documentation/K-Lapse and move files around for a cleaner commit]
    Signed-off-by: kingbri <bdashore3@gmail.com>
    Signed-off-by: LibXZR <xzr467706992@163.com>
    Signed-off-by: KyuoFoxHuyu <88403766@qq.com>

---
+++ make.sh
@@ -0,0 +1,222 @@
+#!/bin/bash
+
+#====================
+sourcecheck=0
+logcat=1
+devicecheck=1
+ABdevice=1
+autopack=0
+#====================
+buildtype=clang
+timezone="Asia/Shanghai"
+build_device="perseus"
+kernel_name="lulu-kernel"
+defconfig_path="perseus_defconfig"
+kbuild_build_user="perseus"
+kbuild_build_host="luluz"
+support="11"
+#====================
+
+print (){
+case ${2} in
+	"red")
+	echo -e "\033[31m $1 \033[0m";;
+
+	"sky")
+	echo -e "\033[36m $1 \033[0m";;
+
+	"green")
+	echo -e "\033[32m $1 \033[0m";;
+
+	*)
+	echo $1
+	;;
+	esac
+}
+
+if [ ${sourcecheck} -eq 1 ];then
+    apt-get update
+    apt-get install -y build-essential bc python curl git zip ftp gcc-aarch64-linux-gnu gcc-arm-linux-gnueabi libssl-dev lftp wget libfl-dev 
+fi
+
+if [ ${sourcecheck} -eq 1 ];then
+    if [ -d "${HOME}/Anykernel3" ];then
+        rm -rf ${HOME}/Anykernel3
+        git clone --depth=1 https://hub.fastgit.xyz/Dedrimer/Anykernel3 ${HOME}/Anykernel3
+    else
+        git clone --depth=1 https://hub.fastgit.xyz/Dedrimer/Anykernel3 ${HOME}/Anykernel3
+    fi
+fi
+
+if [ ${sourcecheck} -eq 1 ];then
+    if [ -d "${HOME}/cbl" ];then
+        rm -rf ${HOME}/cbl
+        git clone --depth=1 https://hub.fastgit.xyz/kdrag0n/proton-clang ${HOME}/cbl
+    else
+        git clone --depth=1 https://hub.fastgit.xyz/kdrag0n/proton-clang ${HOME}/cbl
+    fi
+fi
+
+source=`pwd`
+START_TIME=$(date +"%s")
+date="`date +"%m%d%H%M"`"
+timedatectl set-timezone ${timezone}
+
+print "You are building version:${date}" yellow
+
+clang_path="${HOME}/cbl/bin/"
+gcc_path="/usr/bin/aarch64-linux-gnu-"
+gcc_32_path="/usr/bin/arm-linux-gnueabi-"
+
+args="-j$(nproc --all)  \
+            O=out  \
+            ARCH=arm64 "
+
+if [ ${buildtype} == gcc ];then
+    export PATH=$PATH:/usr/bin/aarch64-linux-gnu-
+    args+="-Wno-unused-function \
+    SUBARCH=arm64 \
+    CROSS_COMPILE=aarch64-linux-gnu- \
+    CROSS_COMPILE_ARM32=arm-linux-gnueabi- "
+else [ ${buildtype} == clang ]
+    args+="CC=${clang_path}/clang \
+	    CLANG_TRIPLE=aarch64-linux-gnu- \
+	    CROSS_COMPILE=${gcc_path} \
+	    AR=${clang_path}/llvm-ar \
+	    NM=${clang_path}/llvm-nm \
+	    OBJCOPY=${clang_path}/llvm-objcopy \
+	    OBJDUMP=${clang_path}/llvm-objdump \
+	    STRIP=${clang_path}/llvm-strip \
+     CROSS_COMPILE_ARM32=${gcc_32_path} "
+fi
+
+
+building(){
+    export KBUILD_BUILD_USER=${kbuild_build_user}
+    export KBUILD_BUILD_HOST=${kbuild_build_host}
+    print "Building Kernel for ${build_device}..." sky
+    if [ ${logcat} -eq 1 ];then
+        make ${args} ${defconfig_path}&&make ${args} 2>&1 | tee out/kernel.log
+        if [ $? = 0 ];then
+            echo -e "\033[32m [INFO] Build successfully \033[0m"
+        else
+            echo -e "\033[31m [ERROR] Build filed \033[0m"
+            exit 1
+        fi
+    else
+        make ${args} ${defconfig_path}&&make ${args}
+        if [ $? = 0 ];then
+            echo -e "\033[32m [INFO] Build successfully \033[0m"
+        else
+            echo -e "\033[31m [ERROR] Build filed \033[0m"
+            exit 1
+        fi
+    fi
+}
+
+building_unclean(){
+	if [ ${logcat} -eq 1 ];then
+        make ${args} 2>&1 | tee out/kernel.log
+        if [ $? = 0 ];then
+            echo -e "\033[32m [INFO] Build successfully \033[0m"
+        else
+            echo -e "\033[31m [ERROR] Build filed \033[0m"
+            exit 1
+        fi
+    else
+        make ${args}
+        if [ $? = 0 ];then
+            echo -e "\033[32m [INFO] Build successfully \033[0m"
+        else
+            echo -e "\033[31m [ERROR] Build filed \033[0m"
+            exit 1
+        fi
+    fi
+
+}
+
+zipfile(){
+    zipfiles="${kernel_name}-${date}.zip"
+    cp -f out/arch/arm64/boot/Image.gz-dtb ~/Anykernel3
+    cd ~/Anykernel3
+    sed -i "s|kernel.string=|kernel.string=${kernel_name}|" "anykernel.sh"
+    sed -i "s|do.devicecheck=|do.devicecheck=${devicecheck}|" "anykernel.sh"
+    sed -i "s|device.name1=|device.name1=${build_device}|" "anykernel.sh"
+    sed -i "s|supported.versions=|supported.versions=${support}|" "anykernel.sh"
+    if [ ${ABdevice} -eq 1 ];then
+        sed -i "s|is_slot_device=|is_slot_device=1|" "anykernel.sh"
+    else
+        sed -i "s|is_slot_device=|is_slot_device=0|" "anykernel.sh"
+    fi
+    zip -r "${zipfiles}" *
+    mv -f "${zipfiles}" ${HOME}
+    sed -i "s|kernel.string=${kernel_name}|kernel.string=|" "anykernel.sh"
+    sed -i "s|do.devicecheck=${devicecheck}|do.devicecheck=|" "anykernel.sh"
+    sed -i "s|device.name1=${build_device}|device.name1=|" "anykernel.sh"
+    sed -i "s|supported.versions=${support}|supported.versions=|" "anykernel.sh"
+    if [ ${ABdevice} -eq 1 ];then
+        sed -i "s|is_slot_device=1|is_slot_device=|" "anykernel.sh"
+    else
+        sed -i "s|is_slot_device=0|is_slot_device=|" "anykernel.sh"
+    fi
+    cd ${HOME}
+    cd $source
+    print "All done.Find it at ${HOME}/${zipfiles}" green
+}
+
+#if [ -d "${HOME}/cbl" ];then
+#    print "You have lost some important sources,please echo sourcecheck=1 to fix it" red
+#    exit 1
+#fi
+
+if [ -d "out" ];then
+    read -p "There are something you have done.Do you want to rebuild it?" Dev
+        if  [ ${Dev} = y ];then
+            if [ ${autopack} -eq 1 ];then
+                rm -rf out
+                building
+                zipfile
+                END_TIME=`date +%s`
+                EXEC_TIME=$((${END_TIME} - ${START_TIME}))
+                EXEC_TIME=$((${EXEC_TIME}/60))
+                echo "Runtime is: ${EXEC_TIME} min"
+            else
+                rm -rf out
+                building
+                END_TIME=`date +%s`
+                EXEC_TIME=$((${END_TIME} - ${START_TIME}))
+                EXEC_TIME=$((${EXEC_TIME}/60))
+                echo "Runtime is: ${EXEC_TIME} min"
+            fi
+        else [ ${Dev} = n ]
+            if [ ${autopack} -eq 1 ];then
+                building_unclean
+                zipfile
+                END_TIME=`date +%s`
+                EXEC_TIME=$((${END_TIME} - ${START_TIME}))
+                EXEC_TIME=$((${EXEC_TIME}/60))
+                echo "Runtime is: ${EXEC_TIME} min"
+            else
+                building_unclean
+                END_TIME=`date +%s`
+                EXEC_TIME=$((${END_TIME} - ${START_TIME}))
+                EXEC_TIME=$((${EXEC_TIME}/60))
+                echo "Runtime is: ${EXEC_TIME} min"
+            fi
+        fi
+else
+    if [ ${autopack} -eq 1 ];then
+        building
+        zipfile
+        END_TIME=`date +%s`
+        EXEC_TIME=$((${END_TIME} - ${START_TIME}))
+        EXEC_TIME=$((${EXEC_TIME}/60))
+        echo "Runtime is: ${EXEC_TIME} min"
+    else
+        building
+        END_TIME=`date +%s`
+        EXEC_TIME=$((${END_TIME} - ${START_TIME}))
+        EXEC_TIME=$((${EXEC_TIME}/60))
+        echo "Runtime is: ${EXEC_TIME} min"
+    fi
+fi
